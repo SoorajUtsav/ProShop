fef11e45c9f738504a3cf7a27e6656362e0b2f05
SoorajUtsav <sooraj.utsav@dltapps.co.uk>
2022-06-25 23:12:06 +0530

    homeScreen added

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,20 @@
+import React from "react";
+import products from "../products";
+import Product from "../components/Product";
+import { Row, Col } from "react-bootstrap";
+const HomeScreen = () => {
+  return (
+    <>
+      <Row>
+        <h1>Latest Products</h1>
+        {products.map((product) => (
+          <Col sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
+
+export default HomeScreen;
