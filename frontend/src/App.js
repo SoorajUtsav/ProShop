514dfb97b2be2b41383846c29286810f78ee78a3
SoorajUtsav <sooraj.utsav@dltapps.co.uk>
2022-06-25 22:22:01 +0530

    header,footer statics added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
 /* eslint-disable no-unused-vars */
 import react from "react";
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <>
-      <h1>Welcome to Proshop</h1>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to Proshop</h1>
+        </Container>
+      </main>
+
+      <Footer />
     </>
   );
 };
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">ProShop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse
+            id="basic-navbar-nav"
+            className="justify-content-end"
+          >
+            <Nav className="mr-auto">
+              <Nav.Link href="/cart">
+                <i className="fas fa-shopping-cart mr-4"></i>Cart
+              </Nav.Link>
+              <Nav.Link href="/login">
+                <i className="fas fa-user mr-4"></i>Sign In
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
