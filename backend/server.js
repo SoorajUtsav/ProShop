c9debd6101178d374886036f73f920fde9de6f3f
SoorajUtsav <sooraj.utsav@dltapps.co.uk>
2023-02-13 08:07:41 +0530

    seeder added

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import colors from "colors";
+import connectDB from "./config/db.js";
 import products from "./data/products.js";
 dotenv.config();
 
 const app = express();
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("API is running.....");
@@ -22,5 +25,7 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
